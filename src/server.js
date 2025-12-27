ba020cc18c338bcf64ca62eb2c9608e1c7ffee68
VitaliiBarabash1990 <alievruslan1818@gmail.com>
2025-12-27 13:49:09 +0100

    add ip

--- src/server.js
+++ src/server.js
@@ -16,10 +16,23 @@ const PORT = Number(env("PORT", "4000"));
 const setupServer = () => {
 	const app = express();
 
-	app.use(express.json());
+	// app.use(express.json());
 	// app.use(cors());
+
+	// app.use(
+	// 	cors({
+	// 		origin: (origin, callback) => {
+	// 			// дозволяємо всі origin (включно з localhost, vercel і т.д.)
+	// 			callback(null, origin || true);
+	// 		},
+	// 		credentials: true,
+	// 	})
+	// );
+
 	const allowedOrigins = [
 		"http://localhost:3000",
+		"http://185.237.206.78",
+		"https://185.237.206.78",
 		"https://estetikmed.vercel.app",
 		"https://estetikmed.pl",
 		"http://estetikmed.pl",
@@ -39,6 +52,10 @@ const setupServer = () => {
 			credentials: true,
 		})
 	);
+
+	app.use(express.json({ limit: "20mb" }));
+	app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+
 	app.use(cookieParser());
 
 	app.use(
